6c34bba31357fcb86a03bd9d80a78b9cb0d66ee3
Joaquin Aguirre <joaquinaguirre17@gmail.com>
2024-06-06 02:09:03 -0300

    Avance en la pagina contacto  y cambio de estilos en distintos componentes

--- Cliente/src/Components/CardsComponents/CardsComponent.jsx
+++ Cliente/src/Components/CardsComponents/CardsComponent.jsx
@@ -2,7 +2,7 @@ import './CardsComponent.css';
 
 function CardsComponent({ titulo, img, descrip }) {
     return (
-        <div className="card mb-3">
+        <div className="card mb-3 hover-card">
             <div className="row g-0">
                 <div className="col-md-4">
                     <img src={img} className="img-fluid rounded-start" alt={titulo} />
--- Cliente/src/Components/FooterComponen/FooterComponent.jsx
+++ Cliente/src/Components/FooterComponen/FooterComponent.jsx
@@ -5,15 +5,18 @@ function FooterComponent() {
             <footer >
                 <p>&copy; 2024 Lia Nails. Todos los derechos reservados.</p>
                 <p>
-                    <a href="/privacidad">Política de Privacidad</a> |
-                    <a href="/terminos">Términos de Servicio</a> |
-                    <a href="/contacto">Contacto</a>
+                    <a >Política de Privacidad</a> |
+                    <a > Términos de Servicio</a> 
+                    
                 </p>
-                <div>
-                    <a href="https://www.facebook.com/tuempresa" target="_blank">Facebook</a> |
-                    <a href="https://www.twitter.com/tuempresa" target="_blank">Twitter</a> |
-                    <a href="https://www.instagram.com/tuempresa" target="_blank">Instagram</a>
-                </div>
+                <ul className="social-list">
+                    <li>
+                        <a href="https://www.instagram.com/lia_nails_manicure?igsh=MTJoOXN0ODdyeHk5YQ==" target="_blank"><i className="fab fa-instagram"></i> </a>
+                    </li>
+                    <li>
+                        <a href="https://api.whatsapp.com/send?phone=543513430611&text=Hola%20buen%20d%C3%ADa%20" target="_blank"><i className="fab fa-whatsapp"></i> </a>
+                    </li>
+                </ul>
             </footer>
         </>
     )
