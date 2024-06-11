2d5b3123e8dd2842c0347cce290baf3a2b39c5ab
Joaquin Aguirre <joaquinaguirre17@gmail.com>
2024-06-11 14:11:42 -0300

    ultimos cambios en el front

--- Cliente/src/Components/FooterComponen/FooterComponent.jsx
+++ Cliente/src/Components/FooterComponen/FooterComponent.jsx
@@ -11,15 +11,16 @@ function FooterComponent() {
                 <h4>Lia Manicure</h4>
                 <ul class="social-list">
                     <li>
-                        <a href="https://www.instagram.com/lia_nails_manicure?igsh=MTJoOXN0ODdyeHk5YQ==" target="_blank" class="social-link">
-                            <i class="fab fa-instagram"></i>
-                        </a>
-                    </li>
-                    <li>
                         <a href="https://api.whatsapp.com/send?phone=543513430611&text=Hola%20buen%20d%C3%ADa%20" target="_blank" class="social-link">
                             <i class="fab fa-whatsapp"></i>
                         </a>
                     </li>
+                    <li>
+                        <a href="https://www.instagram.com/lia_nails_manicure?igsh=MTJoOXN0ODdyeHk5YQ==" target="_blank" class="social-link">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                    </li>
+
                 </ul>
                 <h4>Diseñado por:</h4>
                 <ul class="contact-list">
@@ -36,12 +37,13 @@ function FooterComponent() {
                     <p class="contact-name">David Arce</p>
                     <li class="contact-item">
 
-                        <a href="https://www.instagram.com/david_lbe_37?igsh=b2sxNGYwcTIxcjR4" target="_blank" class="contact-link">
-                            <i class="fab fa-instagram"></i>
-                        </a>
                         <a href="https://api.whatsapp.com/send?phone=+5493517050225&text=Hola%20buen%20d%C3%ADa%20" target="_blank" class="contact-link">
                             <i class="fab fa-whatsapp"></i>
                         </a>
+                        <a href="https://www.instagram.com/david_lbe_37?igsh=b2sxNGYwcTIxcjR4" target="_blank" class="contact-link">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+
                     </li>
                 </ul>
             </footer>
--- Cliente/src/Components/NavarComponent/NavarComponent.jsx
+++ Cliente/src/Components/NavarComponent/NavarComponent.jsx
@@ -9,7 +9,7 @@ function NavarComponent() {
                 <div className="container-fluid">
                     <a className="navbar-brand" href="/">
                         <img src="../../../public/img/Logo.png" alt="Logo" />
-                        <p>Lia Manicure</p>
+                        <p>Lia Nails Manicure</p>
                     </a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
